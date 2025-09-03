6fcdb20f348ec68aeb9da42fc5f60b3a5d8eb991
Damian Koski <damian.koski97@gmail.com>
2025-09-03 22:39:36 +0200

    Setup project files

---
+++ src/OptiForge.AppHost/AppHost.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var server = builder.AddProject<Projects.OptiForge_Server>("server");
+
+var clientHost = builder.AddProject<Projects.OptiForge_ClientHost>("client-host")
+    .WithReference(server)
+    .WaitFor(server);
+
+builder.AddNpmApp("client", "../OptiForge.Client", "dev")
+    .WithHttpEndpoint(env: "PORT")
+    .WithReference(clientHost)
+    .WaitFor(clientHost);
+
+builder.Build().Run();
---
+++ src/OptiForge.ClientHost/Form1.cs
@@ -0,0 +1,10 @@
+namespace OptiForge.ClientHost
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
