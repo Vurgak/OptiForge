dc30a680a1870e35be6a259754d31c0e4df9626a
Damian Koski <damian.koski97@gmail.com>
2025-09-04 22:24:06 +0200

    Make client host a tray only application

---
+++ src/OptiForge.ClientHost/TrayContext.cs
@@ -0,0 +1,17 @@
+﻿namespace OptiForge.ClientHost;
+
+public class TrayContext : ApplicationContext
+{
+    private NotifyIcon _notifyIcon = new();
+
+    public TrayContext()
+    {
+        var menuStrip = new ContextMenuStrip();
+        menuStrip.Items.Add("Exit", null, (sender, e) => Application.Exit());
+
+        _notifyIcon.Text = "OptiForge";
+        _notifyIcon.Icon = new Icon(Path.Combine(AppContext.BaseDirectory, "favicon.ico"));
+        _notifyIcon.ContextMenuStrip = menuStrip;
+        _notifyIcon.Visible = true;
+    }
+}
