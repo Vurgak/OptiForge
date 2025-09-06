ee81e8b4f9487a5fc1c1906bc5b0ee9f476840b3
Damian Koski <damian.koski97@gmail.com>
2025-09-06 20:10:23 +0200

    Fix Client Host in App Host

--- src/OptiForge.AppHost/AppHost.cs
+++ src/OptiForge.AppHost/AppHost.cs
@@ -3,8 +3,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 var server = builder.AddProject<Projects.OptiForge_Server>("server");
 
 var clientHost = builder.AddProject<Projects.OptiForge_ClientHost>("client-host")
-    .WithHttpsEndpoint(6101)
-    .WithHttpEndpoint(6100)
     .WithReference(server)
     .WaitFor(server);
 
